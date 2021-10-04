c862bb416ff81a90a1605719b7792ffd984e6d5e
JunJieYong <jj.yongjunjie@gmail.com>
2021-10-04 21:50:35 +0800

    Starting with some test

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,29 +2,42 @@ import { ReactElement, useEffect } from 'react';
 import './Home.css';
 import PreviewNoteCard from '../../components/PreviewNoteCard';
 import EditNoteModal from '../../components/EditNoteModal';
-import {  useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchNotes, notesSelector, NotesStatus } from '../../slices/noteSlice';
 import DeleteNoteModal from '../../components/DeleteNoteModal';
+import { motion, Target, TargetAndTransition } from 'framer-motion';
 
 function Home(): ReactElement {
   const dispatch = useAppDispatch();
   const { notes: allNotes, status } = useAppSelector(notesSelector);
 
   useEffect(() => {
-    dispatch(fetchNotes())
-  }, [dispatch])
+    dispatch(fetchNotes());
+  }, [dispatch]);
+
+  const gridInital: Target = {
+    display: 'flex',
+    flexFlow: 'row wrap'
+  }
+
+  const gridAnimate: TargetAndTransition  = {
+    
+  }
 
   //TODO: Search
   return (
-    <div className=''>
-      <div className='notes-grid'>
-        {allNotes.map((note, index) => (
-          <PreviewNoteCard {...{ key: index, note, index }} />
-        ))}
-      </div>
+    <motion.div
+      layout 
+      initial={gridInital}
+      animate={gridAnimate}
+      className='notes-grid'
+    >
+      {allNotes.map((note, index) => (
+        <PreviewNoteCard {...{ key: index, note, index }} />
+      ))}
       {status === NotesStatus.Editing ? <EditNoteModal /> : <div />}
       {status === NotesStatus.Deleting ? <DeleteNoteModal /> : <div />}
-    </div>
+    </motion.div>
   );
 }
 
