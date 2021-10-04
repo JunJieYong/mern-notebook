5f95af47e30d2cc12489f4f59bafc95b5e192a22
JunJieYong <jj.yongjunjie@gmail.com>
2021-10-04 22:59:39 +0800

    Preperation for heroku deployment

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,10 @@ app.use(express.json());
 app.use('/api/notes', notesRouter);
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.redirect('/index.html'));
+}
+
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () =>
-  logger.info(`Server is running on port ${PORT}`)
-);
+const server = app.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
