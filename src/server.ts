1cbc1b283c802d7ba768a654bf1408963ce5bdb9
JunJie Yong <13977709+JunJieYong@users.noreply.github.com>
2021-10-05 19:38:13 +0800

    Revert "Websockets"

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,19 @@
 import { config } from 'dotenv';
 config();
 import { connectDB } from './config/db';
-import { createServer } from 'http';
 import express from 'express';
-import { Server } from 'socket.io';
 import errorHandler from './middlewares/error';
 import notesRouter from './routes/notes';
 import logger from './utils/logger';
-import { registerNotesHandler } from './ioHandlers/notes';
 
 connectDB();
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
 app.use(express.json());
 
 app.use('/api/notes', notesRouter);
 app.use(errorHandler);
-
-io.on('connection', socket => {
-  registerNotesHandler(io, socket);
-});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -30,4 +21,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
+const server = app.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
