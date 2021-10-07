294c8b2749cfb69bebdf0d5c59c9cfc3d34da11d
JunJieYong <jj.yongjunjie@gmail.com>
2021-10-08 00:17:22 +0800

    Completed Grid Layout and animations

--- client/src/components/PreviewGrid/index.tsx
+++ client/src/components/PreviewGrid/index.tsx
@@ -1,19 +1,12 @@
 import { ReactElement, useEffect, useRef, useState } from 'react';
 import './PreviewGrid.css';
-import { Notes } from '../../models/notes';
+import { IdedNotes, Notes } from '../../models/notes';
 import { motion, Target, TargetAndTransition } from 'framer-motion';
 
 const minimumWidth = 240;
 const minimumGap = 16;
 interface GridProps {
-  notes: Notes[];
-}
-
-interface UICardData {
-  note: Notes;
-  heightPromise?: Promise<number>;
-  contentHeight?: number;
-  heightCallback?: (height: number) => void;
+  notes: IdedNotes[];
 }
 
 interface ColumnData {
@@ -22,121 +15,124 @@ interface ColumnData {
   gap: number;
 }
 
-interface Dimension {
-  visible: boolean;
+interface CardPosition {
+  noteId: string;
+  x: number;
+  y: number;
+}
+
+type HeightPromise = Promise<number>;
+type HeightCallback = (height: number) => void;
+
+interface CardResolver {
+  noteId: string;
+  resolver: HeightCallback;
+}
+
+interface CardHeight {
+  noteId: string;
   height: number;
+}
+
+interface CardProps {
+  note: Notes;
   width: number;
-  posx: number;
-  posy: number;
+  visible: boolean;
+  initialY: number;
+  position?: Omit<CardPosition, 'noteId'>;
+  heightCallback?: HeightCallback;
 }
 
 function PreviewGrid({ notes }: GridProps): ReactElement {
   const [gridWidth, setGridWidth] = useState<number>();
   const [columnData, setColumnData] = useState<ColumnData>({ width: minimumWidth, gap: minimumGap, quantity: 1 });
-  const [cardsData, setCardsData] = useState<UICardData[]>([]);
-  const [dimensions, setDimensions] = useState<Dimension[]>([]);
+  const [cardResolvers, setCardResolvers] = useState<CardResolver[]>([]);
+  const [cardsHeight, setCardsHeight] = useState<CardHeight[]>([]);
   const previewGrid = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log('Setting up width changed callback');
     const resizeCallback = () => setGridWidth(previewGrid.current?.offsetWidth);
     window.addEventListener('resize', resizeCallback);
     resizeCallback();
-    return () => {
-      console.log('Removed Resize listener');
-      window.removeEventListener('resize', resizeCallback);
-    };
+    return () => window.removeEventListener('resize', resizeCallback);
   }, [setGridWidth]);
 
   useEffect(() => {
-    console.log('Notes Changed');
-
-    const { heightPromises, cardDatas } = notes.reduce(
-      (accumulator, note, index) => {
-        let resolver: (h: number) => void = () => {};
-        //prettier-ignore
-        const { contentHeight: prevHeight, heightCallback: prevCallback, heightPromise: prevPromise } = 
-          cardsData[index] ?? { contentHeight: undefined, heightCallback: undefined, heightPromise: undefined };
-
-        const heightCallback =
-          prevCallback !== undefined ? undefined : prevCallback ? prevCallback : (h: number) => resolver(h);
-        const heightPromise =
-          prevHeight !== undefined
-            ? Promise.resolve(prevHeight)
-            : prevPromise
-            ? prevPromise
-            : new Promise<number>(resolve => (resolver = resolve));
-
-        const cardData: UICardData = { note, heightCallback, heightPromise, contentHeight: prevHeight };
-        accumulator.heightPromises.push(heightPromise);
-        accumulator.cardDatas.push(cardData);
-        return accumulator;
-      },
-      { heightPromises: [], cardDatas: [] } as { heightPromises: Promise<number>[]; cardDatas: UICardData[] },
-    );
-
-    (window as any).hp = heightPromises;
-    setCardsData(cardDatas);
-
-    Promise.all(heightPromises).then(heights =>
-      setCardsData(cardDatas.map((cd, i) => ({ ...cd, contentHeight: heights[i] }))),
-    );
-  }, [notes.map(({ title, content }) => `${title.length},${content.length}`).join()]);
+    if (gridWidth) {
+      const quantity = Math.floor(gridWidth / (minimumWidth + minimumGap));
+      const extras = (gridWidth % (minimumWidth + minimumGap)) / quantity;
+      const width = minimumWidth + Math.floor(extras);
+      const gap = minimumGap;
+      setColumnData({ gap, width, quantity });
+    }
+  }, [gridWidth]);
 
   useEffect(() => {
-    console.log(`Content Height / Width Changed`);
+    const heightPromises: [string, Promise<number>][] = [];
+    const newResolvers = [...cardResolvers];
+    notes.forEach(note => {
+      let resolver: (h: number) => void = () => {};
+      const callbackIndex = newResolvers.findIndex(prev => prev.noteId === note._id);
+      const promise = new Promise<number>(resolve => (resolver = resolve));
+      heightPromises.push([note._id, promise]);
 
-    if (!gridWidth) {
-      console.log(`Missing Grid Width`);
-      return;
-    }
-
-    (window as any).notesContentHeight = cardsData.map(({ contentHeight }) => !contentHeight);
-    if (cardsData.some(({ contentHeight }) => !contentHeight)) {
-      console.log(`Missing Content Height`);
-      if (cardsData.reduce((a, { contentHeight }, i) => (!contentHeight ? [...a, i] : a), [] as number[]).length > 4)
-        debugger;
-      return;
-    }
-
-    console.log('Calculating Position');
-
-    const numCol = Math.floor(gridWidth / 240);
-    console.log(`Number of Columns: ${numCol}`);
-
-    const heights = new Array(numCol).fill(0);
-    const dims = cardsData.map<Dimension>(card => {
-      const lowestValueIndex = heights.reduce((lvi, v, i, a) => (v < a[lvi] ? i : lvi), 0);
-      const dim = {
-        visible: true,
-        posx: lowestValueIndex * (240 + 16),
-        posy: heights[lowestValueIndex],
-        width: 240,
-        height: card.contentHeight ?? 0,
-      };
-      heights[lowestValueIndex] += card.contentHeight ? card.contentHeight + 16 : 0;
-      return dim;
+      if (callbackIndex > -1) newResolvers[callbackIndex] = { noteId: note._id, resolver };
+      else newResolvers.push({ noteId: note._id, resolver });
     });
-    setDimensions(dims);
-  }, [gridWidth, cardsData.map(({ contentHeight }) => contentHeight).join()]);
+
+    setCardResolvers(newResolvers);
+
+    Promise.all(heightPromises.map(([noteId, promise]) => promise.then(height => ({ noteId, height })))).then(
+      heights => {
+        const newCardsHeight = [...cardsHeight];
+        heights.forEach(cur => {
+          const index = newCardsHeight.findIndex(prev => cur.noteId === prev.noteId);
+          if (index > -1) newCardsHeight[index].height = cur.height;
+          else newCardsHeight.push({ ...cur });
+        });
+        setCardsHeight(newCardsHeight);
+      },
+    );
+  }, [notes, columnData.width, columnData.quantity, columnData.gap]);
+
+  const renderCards = () => {
+    const colHeights = new Array(columnData.quantity).fill(0);
+    const cards = notes.map((note, index) => {
+      const resolverIndex = cardResolvers.findIndex(({ noteId }) => noteId === note._id);
+      const resolver = resolverIndex > -1 ? cardResolvers[resolverIndex].resolver : undefined;
+
+      const heightIndex = cardsHeight.findIndex(({ noteId }) => noteId === note._id);
+      const height = heightIndex > -1 ? cardsHeight[heightIndex].height : 0;
+      const lowestValueIndex = colHeights.reduce((lvi, v, i, a) => (v < a[lvi] ? i : lvi), 0);
+      const x = lowestValueIndex * (columnData.width + columnData.gap);
+      const y = colHeights[lowestValueIndex];
+      colHeights[lowestValueIndex] += height + 16;
+      return (
+        <PreviewCard
+          key={index}
+          note={note}
+          initialY={y}
+          visible={!!height}
+          position={{ x, y }}
+          width={columnData.width}
+          heightCallback={resolver}
+        />
+      );
+    });
+
+    return cards;
+  };
 
   return (
     <motion.div ref={previewGrid} className='preview-grid'>
-      {cardsData.map((card, index) => (
-        <PreviewCard key={index} {...card} dim={dimensions[index]} />
-      ))}
+      {renderCards()}
     </motion.div>
   );
 }
 
-interface CardProps {
-  note: Notes;
-  heightCallback?: (height: number) => void;
-  dim?: Dimension;
-}
-
-function PreviewCard({ note, heightCallback, dim }: CardProps): ReactElement<CardProps> {
+function PreviewCard({ note, heightCallback, width, initialY, visible, position }: CardProps): ReactElement<CardProps> {
   const [animateProperty, setAnimateProperty] = useState<Target>();
+  const visibility = visible ? 'visible' : 'hidden';
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (ref.current) {
@@ -145,25 +141,15 @@ function PreviewCard({ note, heightCallback, dim }: CardProps): ReactElement<Car
         heightCallback(ref.current.offsetHeight);
       }
     }
-  }, [ref.current, heightCallback]);
-
-  useEffect(() => {
-    //When dim or dim?.posx or dim?.posy or dim?.width or dim?.height changed
-    //Set the animate property
-    setAnimateProperty({
-      x: dim?.posx,
-      y: dim?.posy,
-      width: dim?.width,
-      height: dim?.height,
-      visibility: dim?.visible ? 'visible' : 'hidden',
-    });
-  }, [dim, dim?.posx, dim?.posy, dim?.width, dim?.height]);
+  }, [ref.current, note, heightCallback]);
 
   return (
     <motion.div
       ref={ref}
       className='preview-card'
-      animate={animateProperty} //Use the animate property
+      style={{ width }}
+      initial={{ y: initialY }}
+      animate={{ ...position, visibility }} //Use the animate property
       transition={{
         damping: 100,
       }}
